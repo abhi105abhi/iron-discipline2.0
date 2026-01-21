930a298e8cab93f18fe66e61ce439eda9ce2abe6
abhi105abhi <42896602+abhi105abhi@users.noreply.github.com>
2026-01-21 15:10:39 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,16 +2,19 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// Firebase console se details yahan paste kariyo
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "iron-discipline.firebaseapp.com",
-  projectId: "iron-discipline",
-  storageBucket: "iron-discipline.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyC3zUkXcT-oQjvCpMLb3caIYsmSWoGWPDY",
+  authDomain: "sudhar-ja.firebaseapp.com",
+  projectId: "sudhar-ja",
+  storageBucket: "sudhar-ja.firebasestorage.app",
+  messagingSenderId: "893000798529",
+  appId: "1:893000798529:web:5e70481b524802dae0d1c0",
+  measurementId: "G-XG6F9M46GB"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Export instances for the rest of the app
 export const db = getFirestore(app);
 export const auth = getAuth(app);
